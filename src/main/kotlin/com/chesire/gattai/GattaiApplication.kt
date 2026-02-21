58eea09b4e4ec61352e1e500009632d00918f011
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-02-21 16:16:43 +0000

    Initial commit

---
+++ src/main/kotlin/com/chesire/gattai/GattaiApplication.kt
@@ -0,0 +1,11 @@
+package com.chesire.gattai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GattaiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GattaiApplication>(*args)
+}
---
+++ src/test/kotlin/com/chesire/gattai/GattaiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chesire.gattai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GattaiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
