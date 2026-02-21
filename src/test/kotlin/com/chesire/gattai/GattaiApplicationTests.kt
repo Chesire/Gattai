04d86a21cff778948097b8de9d85a7a644372abf
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-02-21 22:25:19 +0000

    Add assertTrue to ensure single test works

--- src/test/kotlin/com/chesire/gattai/GattaiApplicationTests.kt
+++ src/test/kotlin/com/chesire/gattai/GattaiApplicationTests.kt
@@ -7,5 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 class GattaiApplicationTests {
 
     @Test
-    fun contextLoads()
+    fun contextLoads() {
+        assert(true)
+    }
 }
