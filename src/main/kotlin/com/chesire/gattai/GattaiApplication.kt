e5dcee7edcd443404228dc287edd7c27e179c7db
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-02-23 21:17:46 +0000

    Add suppression for spread in args

--- src/main/kotlin/com/chesire/gattai/GattaiApplication.kt
+++ src/main/kotlin/com/chesire/gattai/GattaiApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class GattaiApplication
 
 fun main(args: Array<String>) {
+    @Suppress("SpreadOperator")
     runApplication<GattaiApplication>(*args)
 }
