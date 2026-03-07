87882a92c438437a6ba32df7231dcc79840b8740
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-07 14:44:22 +0000

    feat: add initial series domain

---
+++ src/main/kotlin/com/chesire/gattai/domain/Series.kt
@@ -0,0 +1,14 @@
+package com.chesire.gattai.domain
+
+data class Series(
+    val kitsuId: Int?,
+    val malId: Int?,
+    val aniListId: Int?,
+    val title: String,
+    val seriesType: SeriesType
+)
+
+enum class SeriesType {
+    Anime,
+    Manga
+}
