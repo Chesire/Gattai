e7ce6cd9917e69799cac8971bba201b0241eba6a
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-07 19:09:16 +0000

    feat: return the entity from search

--- src/main/kotlin/com/chesire/gattai/feature/search/SearchController.kt
+++ src/main/kotlin/com/chesire/gattai/feature/search/SearchController.kt
@@ -1,5 +1,7 @@
 package com.chesire.gattai.feature.search
 
+import com.chesire.gattai.domain.Series
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 class SearchController(private val aggregator: SearchAggregator) {
 
     @GetMapping
-    fun findSeries(@RequestParam params: SearchParams) {
+    fun findSeries(@RequestParam params: SearchParams): ResponseEntity<List<Series>> {
         val series = aggregator.findSeries(params)
+        return ResponseEntity.ok(series)
     }
 }
