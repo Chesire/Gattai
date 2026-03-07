5d5cd66814744979525f64c10dfb7fd01d7e4ad5
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-07 20:12:40 +0000

    Add methods to retrieve the mapped ids

--- src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingProvider.kt
+++ src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingProvider.kt
@@ -2,22 +2,23 @@ package com.chesire.gattai.provider.mapping
 
 class SeriesIdMappingProvider {
 
+    private val _byKitsuId: Map<Int, SeriesIdMappingEntry>
+    private val _byMalId: Map<Int, SeriesIdMappingEntry>
+    private val _byAnilistId: Map<Int, SeriesIdMappingEntry>
+
+    private val _entries: List<SeriesIdMappingEntry> = listOf(
+        SeriesIdMappingEntry(kitsuId = 1, malId = 1, anilistId = 1),
+        SeriesIdMappingEntry(kitsuId = 2, malId = 2, anilistId = 2),
+        SeriesIdMappingEntry(kitsuId = 3, malId = 3, anilistId = 3)
+    )
+
     init {
-        // Load the items into memory, maybe make an async function to do this
+        _byKitsuId = _entries.filter { it.kitsuId != null }.associateBy { it.kitsuId!! }
+        _byMalId = _entries.filter { it.malId != null }.associateBy { it.malId!! }
+        _byAnilistId = _entries.filter { it.anilistId != null }.associateBy { it.anilistId!! }
     }
 
-    fun findByKitsuId(kitsuId: Int): SeriesIdMappingEntry? {
-        // Find the entry by the kitsu id
-        return null
-    }
-
-    fun findByMalId(malId: Int): SeriesIdMappingEntry? {
-        // Find the entry by the mal id
-        return null
-    }
-
-    fun findByAnilistId(anilistId: Int): SeriesIdMappingEntry? {
-        // Find the entry by the anilist id
-        return null
-    }
+    fun findByKitsuId(kitsuId: Int): SeriesIdMappingEntry? = _byKitsuId[kitsuId]
+    fun findByMalId(malId: Int): SeriesIdMappingEntry? = _byMalId[malId]
+    fun findByAnilistId(anilistId: Int): SeriesIdMappingEntry? = _byAnilistId[anilistId]
 }
