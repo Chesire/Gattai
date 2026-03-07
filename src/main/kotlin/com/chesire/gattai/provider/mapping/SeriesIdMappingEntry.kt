28baa932510d08599142b978d266d7f541280cb1
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-07 19:56:01 +0000

    Create initial provider and data model

---
+++ src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingEntry.kt
@@ -0,0 +1,7 @@
+package com.chesire.gattai.provider.mapping
+
+data class SeriesIdMappingEntry(
+    val kitsuId: Int? = null,
+    val malId: Int? = null,
+    val anilistId: Int? = null
+)
---
+++ src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingProvider.kt
@@ -0,0 +1,23 @@
+package com.chesire.gattai.provider.mapping
+
+class SeriesIdMappingProvider {
+
+    init {
+        // Load the items into memory, maybe make an async function to do this
+    }
+
+    fun findByKitsuId(kitsuId: Int): SeriesIdMappingEntry? {
+        // Find the entry by the kitsu id
+        return null
+    }
+
+    fun findByMalId(malId: Int): SeriesIdMappingEntry? {
+        // Find the entry by the mal id
+        return null
+    }
+
+    fun findByAnilistId(anilistId: Int): SeriesIdMappingEntry? {
+        // Find the entry by the anilist id
+        return null
+    }
+}
