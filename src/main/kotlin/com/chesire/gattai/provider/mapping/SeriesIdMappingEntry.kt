f07565ce7ce7c1082e211e25d66ddc63a1308d17
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-07 20:28:41 +0000

    Add JsonProperty to the entry fields

--- src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingEntry.kt
+++ src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingEntry.kt
@@ -1,7 +1,12 @@
 package com.chesire.gattai.provider.mapping
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class SeriesIdMappingEntry(
+    @JsonProperty("kitsu_id")
     val kitsuId: Int? = null,
+    @JsonProperty("mal_id")
     val malId: Int? = null,
+    @JsonProperty("anilist_id")
     val anilistId: Int? = null
 )
