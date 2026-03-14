cdd463b409b91e1f37cfb5d2b3438437062e8656
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-14 15:26:36 +0000

    Load the list into memory on application start

--- src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingProvider.kt
+++ src/main/kotlin/com/chesire/gattai/provider/mapping/SeriesIdMappingProvider.kt
@@ -1,18 +1,27 @@
 package com.chesire.gattai.provider.mapping
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Component
+
+@Component
 class SeriesIdMappingProvider {
 
     private val _byKitsuId: Map<Int, SeriesIdMappingEntry>
     private val _byMalId: Map<Int, SeriesIdMappingEntry>
     private val _byAnilistId: Map<Int, SeriesIdMappingEntry>
 
-    private val _entries: List<SeriesIdMappingEntry> = listOf(
-        SeriesIdMappingEntry(kitsuId = 1, malId = 1, anilistId = 1),
-        SeriesIdMappingEntry(kitsuId = 2, malId = 2, anilistId = 2),
-        SeriesIdMappingEntry(kitsuId = 3, malId = 3, anilistId = 3)
-    )
+    private val _entries: List<SeriesIdMappingEntry>
 
     init {
+        ClassPathResource("mapping/anime-list-mini.json")
+            .inputStream
+            .use { inputStream ->
+                _entries = jacksonObjectMapper()
+                    .readValue(inputStream, Array<SeriesIdMappingEntry>::class.java)
+                    .toList()
+            }
+
         _byKitsuId = _entries.filter { it.kitsuId != null }.associateBy { it.kitsuId!! }
         _byMalId = _entries.filter { it.malId != null }.associateBy { it.malId!! }
         _byAnilistId = _entries.filter { it.anilistId != null }.associateBy { it.anilistId!! }
