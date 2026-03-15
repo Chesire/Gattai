2b486e660f481b6e2127d1328fd632122af745be
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-15 23:05:04 +0000

    Protect against graphql injection

--- src/main/kotlin/com/chesire/gattai/provider/anilist/AnilistSearchService.kt
+++ src/main/kotlin/com/chesire/gattai/provider/anilist/AnilistSearchService.kt
@@ -5,6 +5,7 @@ import com.chesire.gattai.domain.search.SearchService
 import com.chesire.gattai.feature.search.SearchModel
 import com.chesire.gattai.feature.search.SearchParams
 import com.chesire.gattai.provider.anilist.dto.AnilistQueryDto
+import com.chesire.gattai.provider.anilist.dto.AnilistQueryVariablesDto
 import com.chesire.gattai.provider.anilist.dto.AnilistSearchDto
 import org.springframework.stereotype.Component
 
@@ -33,19 +34,25 @@ class AnilistSearchService(private val client: AnilistClient) : SearchService {
             .orEmpty() // TODO: Handle better
     }
 
-    private fun buildQuery(params: SearchParams): AnilistQueryDto {
-        return AnilistQueryDto(
-            """
-                query {
-                  Page(perPage: 20) {
-                    media(search: "${params.title}", type: ${params.seriesType}) {
-                      id
-                      title { romaji }
-                      idMal
-                    }
-                  }
-                }
-            """.trimIndent()
+    private fun buildQuery(params: SearchParams) = AnilistQueryDto(
+        query = SEARCH_QUERY,
+        variables = AnilistQueryVariablesDto(
+            search = params.title,
+            type = params.seriesType.toString()
         )
+    )
+
+    companion object {
+        private val SEARCH_QUERY = $$"""
+            query ($search: String, $type: MediaType) {
+              Page(perPage: 20) {
+                media(search: $search, type: $type) {
+                  id
+                  title { romaji }
+                  idMal
+                }
+              }
+            }
+        """.trimIndent()
     }
 }
--- src/main/kotlin/com/chesire/gattai/provider/anilist/dto/AnilistQueryDto.kt
+++ src/main/kotlin/com/chesire/gattai/provider/anilist/dto/AnilistQueryDto.kt
@@ -4,5 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class AnilistQueryDto(
     @JsonProperty("query")
-    val query: String
+    val query: String,
+    @JsonProperty("variables")
+    val variables: AnilistQueryVariablesDto
+)
+
+data class AnilistQueryVariablesDto(
+    @JsonProperty("search")
+    val search: String,
+    @JsonProperty("type")
+    val type: String
 )
