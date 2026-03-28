1af12e88b9ff081404e2d11a19d48e9093fe1d57
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-28 21:38:28 +0000

    Hide tokens in the toString method

--- src/main/kotlin/com/chesire/gattai/domain/Tokens.kt
+++ src/main/kotlin/com/chesire/gattai/domain/Tokens.kt
@@ -8,4 +8,11 @@ data class Tokens(
 
     val hasValidToken: Boolean
         get() = kitsuToken != null || anilistToken != null || malToken != null
+
+    override fun toString(): String {
+        return "Tokens(" +
+            "kitsuToken=${if (kitsuToken != null) "[REDACTED]" else "null"}, " +
+            "anilistToken=${if (anilistToken != null) "[REDACTED]" else "null"}, " +
+            "malToken=${if (malToken != null) "[REDACTED]" else "null"})"
+    }
 }
