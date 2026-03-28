127981df5b4b69d2399639901f35ef1a35a8b71a
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-28 21:13:02 +0000

    Send error if no tokens provided

--- src/main/kotlin/com/chesire/gattai/feature/retrievelibrary/RetrieveLibraryController.kt
+++ src/main/kotlin/com/chesire/gattai/feature/retrievelibrary/RetrieveLibraryController.kt
@@ -1,6 +1,7 @@
 package com.chesire.gattai.feature.retrievelibrary
 
 import com.chesire.gattai.domain.SeriesEntry
+import com.chesire.gattai.error.ErrorResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -17,9 +18,21 @@ class RetrieveLibraryController {
         @RequestHeader(value = "anilist-token", required = false) anilistToken: String?,
         @RequestHeader(value = "mal-token", required = false) malToken: String?
     ): ResponseEntity<Any> {
+        if (!hasValidToken(kitsuToken = kitsuToken, anilistToken = anilistToken, malToken = malToken)) {
+            return ResponseEntity
+                .badRequest()
+                .body(
+                    ErrorResponse("No tokens provided")
+                )
+        }
+
         // Send off to a service
         return ResponseEntity
             .ok()
             .body(listOf<SeriesEntry>())
+    }
+
+    private fun hasValidToken(kitsuToken: String?, anilistToken: String?, malToken: String?): Boolean {
+        return kitsuToken != null || anilistToken != null || malToken != null
     }
 }
