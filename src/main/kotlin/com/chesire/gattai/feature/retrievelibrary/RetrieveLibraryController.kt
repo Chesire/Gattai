22aeaa1efc0172fb42eeb3d36b2f28423f766488
Troy Rijkaard <troy.rijkaard@chesire.dev>
2026-03-28 19:49:50 +0000

    Add initial controller for retrieving library

---
+++ src/main/kotlin/com/chesire/gattai/feature/retrievelibrary/RetrieveLibraryController.kt
@@ -0,0 +1,22 @@
+package com.chesire.gattai.feature.retrievelibrary
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/retrieve-library")
+class RetrieveLibraryController {
+
+    @GetMapping
+    fun retrieveLibrary(
+        @RequestHeader("kitsu-token") kitsuToken: String,
+        @RequestHeader("anilist-token") anilistToken: String,
+        @RequestHeader("mal-token") malToken: String,
+    ): ResponseEntity<Any> {
+        // Send off to a service
+        return ResponseEntity.ok().body(Unit)
+    }
+}
